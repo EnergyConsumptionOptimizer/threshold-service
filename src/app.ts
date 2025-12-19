341bb2ca82e37887e278b4f19f7fc899a5496f9a
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-19 17:47:26 +0100

    refactor: integrate cookie-parser middleware into app setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { errorsHandler } from "@interfaces/web-api/middlewares/ErrorsMiddleware";
 import { apiRouter } from "@interfaces/web-api/dependencies";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(apiRouter);
 app.use(errorsHandler);
