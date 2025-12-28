70d9472fa4c5010d696dccdd1f124b223e4c90c4
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-28 17:57:23 +0100

    feat: add `evaluations/forecast` endpoint in `internalRoutes` for forecast evaluation

---
+++ src/interfaces/web-api/internal/internalRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { ThresholdController } from "@interfaces/web-api/controllers/ThresholdController";
+
+export function internalRoutes(
+  thresholdController: ThresholdController,
+): Router {
+  const router = Router();
+
+  router.post("/evaluations/forecast", thresholdController.evaluateForecast);
+
+  return router;
+}
--- src/interfaces/web-api/routes/router.ts
+++ src/interfaces/web-api/routes/router.ts
@@ -2,6 +2,7 @@ import { ThresholdController } from "@interfaces/web-api/controllers/ThresholdCo
 import { AuthMiddleware } from "@interfaces/web-api/middlewares/AuthMiddleware";
 import { Router } from "express";
 import { ThresholdRouter } from "@interfaces/web-api/routes/thresholdRoutes";
+import { internalRoutes } from "@interfaces/web-api/internal/internalRoutes";
 
 export function router(
   thresholdController: ThresholdController,
@@ -13,6 +14,8 @@ export function router(
     "/api/thresholds",
     ThresholdRouter(thresholdController, authMiddleware),
   );
+
+  router.use("/api/internal", internalRoutes(thresholdController));
 
   return router;
 }
